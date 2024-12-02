3c5a1c706388085aaf55e0343027612d4e1bba7f
Manas Dange <notify.manas@gmail.com>
2024-12-02 14:35:21 +0530

    new commit
    tp

--- Desktop/sm/Student_management_project.py
+++ Desktop/sm/Student_management_project.py
@@ -1,5 +1,5 @@
 class Student:
-    def __init__(self, name, roll_no, marks):
+    def __init__(self, name, roll_no, marks);
         self.name = name
         self.roll_no = roll_no
         self.marks = marks
